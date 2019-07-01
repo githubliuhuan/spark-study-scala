e5578c9bfc0f257673f9a3085186480e500c0d87
liuhuan <13942061310@163.com>
2019-07-01 21:28:18 +0800

    共享变量

---
+++ src/cn/spark/study/core/BroadcastVariable.scala
@@ -0,0 +1,18 @@
+package cn.spark.study.core
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object BroadcastVariable {
+  val conf = new SparkConf()
+    .setAppName("BroadcastVariable")
+    .setAppName("local")
+
+  val sc = new SparkContext(conf)
+  val factor = 3
+  val factorBroadcast = sc.broadcast(factor)
+  val numberArray = Array(1, 2, 3, 4, 5)
+  val numbers = sc.parallelize(numberArray)
+  val multipleNumbers = numbers.map(num => num * factorBroadcast.value)
+
+  multipleNumbers.foreach(num => println(num))
+}
