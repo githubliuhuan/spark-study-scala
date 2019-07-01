23782873e03e57e2f98614b4b09e26678538ccbd
liuhuan <13942061310@163.com>
2019-07-01 21:36:12 +0800

    共享变量（累加变量）

---
+++ src/cn/spark/study/core/AccumulatorVariable.scala
@@ -0,0 +1,21 @@
+package cn.spark.study.core
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object AccumulatorVariable {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName("AccumulatorVariable")
+      .setAppName("local")
+    val sc = new SparkContext(conf)
+
+    val sum = sc.accumulator(0)
+
+    val numberArray = Array(1, 2, 3, 4, 5)
+    val numbers = sc.parallelize(numberArray,1)
+
+    numbers.foreach(num => sum += num)
+    println(sum)
+
+  }
+}
