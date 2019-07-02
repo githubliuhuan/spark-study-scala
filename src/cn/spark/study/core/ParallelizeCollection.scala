f7389b6e57488b200c9d50e10fbc1fd3509e598f
huanliu78 <huanliu78@creditease.cn>
2019-07-02 16:01:38 +0800

    添加作者

--- src/cn/spark/study/core/ParallelizeCollection.scala
+++ src/cn/spark/study/core/ParallelizeCollection.scala
@@ -1,12 +1,15 @@
 package cn.spark.study.core
 import org.apache.spark.{SparkConf, SparkContext}
 
+// @author liuhuan
+
 
 object ParallelizeCollection {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
       .setAppName("ParallelizeCollection")
       .setMaster("local")
+
     val sc = new SparkContext(conf)
 
     val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)
