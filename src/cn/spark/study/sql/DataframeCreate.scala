8f3f4301ffe3bf07cdaf1faf0f325d1728658f6a
liuhuan <13942061310@163.com>
2019-07-06 14:05:37 +0800

    创建dataframe和df常用操作

--- src/cn/spark/study/sql/DataframeCreate.scala
+++ src/cn/spark/study/sql/DataframeCreate.scala
@@ -10,8 +10,20 @@ object DataframeCreate {
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
+    // 创建出来的DataFrame完全可以理解为一张表
     val df = sqlContext.read.json("")
+    // 打印DataFrame所有数据（select * from ...)
     df.show()
+    // 打印DataFrame的元数据（Schema）
+    df.printSchema()
+    // 查询某列所有的数据
+    df.select("name").show()
+    // 查询某几列所有的数据，并对列进行计算
+    df.select(df.col("name"),df.col("age").plus(1)).show()
+    // 根据某一列的值进行过滤
+    df.filter(df.col("age").gt(18)).show()
+    // 根据某一列进行分组，然后进行聚合
+    df.groupBy(df.col("age")).count().show()
 
   }
 }
