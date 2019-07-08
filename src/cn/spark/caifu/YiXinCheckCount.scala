7984546e3ae73e1e093da0f33ddf6708fb61cb00
huanliu78 <huanliu78@creditease.cn>
2019-07-08 18:42:24 +0800

    commit

--- src/cn/spark/caifu/YiXinCheckCount.scala
+++ src/cn/spark/caifu/YiXinCheckCount.scala
@@ -8,13 +8,27 @@ object YiXinCheckCount {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
       .setAppName("YiXinCheckCount")
-
     val sc = new SparkContext(conf)
-
     val hivecontext = new HiveContext(sc)
-    val checkTableDF = hivecontext.sql("select 'dm.dim_crm_customer' as table_name,count(*),from_unixtime(unix_timestamp(),'yyyy-MM-dd HH:mm:ss') as check_time from dm.dim_crm_customer")
+    val sqlUnionAll = readTxt("/opt/program/work/dev/liuhuan/spark-study-scala/spark-sql/monitor_table.txt")
+    val checkTableDF = hivecontext.sql("sqlUnionAll")
     checkTableDF.write.mode(SaveMode.Append).format("hive").saveAsTable("dm.table_count")
     println(checkTableDF.collect().foreach(println))
 
+  }
+
+  def readTxt(filePath: String): String = {
+    import scala.io.Source
+    val file=Source.fromFile(filePath)
+    // getLines返回的是一个迭代器
+    var sqlUnion = ""
+    for(line <- file.getLines)
+    {
+      var sql = s"select '${line}' as table_name,count(*),from_unixtime(unix_timestamp(),'yyyy-MM-dd HH:mm:ss') as check_time from ${line} union all"
+      sqlUnion += sql
+    }
+    val sqlUnionAll = sqlUnion.substring(0,sqlUnion.length - 10)
+    file.close
+    sqlUnionAll
   }
 }
